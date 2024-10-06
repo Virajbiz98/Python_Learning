e85780e43cae9e51ee0d24b6350f8856208c0133
Virajbiz98 <Virajbiz98@gmail.com>
2024-10-06 22:49:16 +0530

    Update python_basics

--- python_basics.py
+++ python_basics.py
@@ -1,1 +1,19 @@
+~ constants ~
+#..Let’s first look at constants. 
+#.So, what are constants?
+#.Why do we call them constants.
+#.We call them constants because their values do not change. They are fixed values. Constants can be numbers, letters or strings.
+#.We can have numeric constants and string constants.
+#.String constants are usually enclosed within single quotes or double quotes. What happens when we pass a constant to the print function?
+#.It basically prints the value of that constant to the console. For example, when we say ‘print(123)’ Python prints ‘123’ onto the console.
+#.When we say print("hellow") - >>> hellow
+
+~ variables ~
+#.Now we move on to introduce variables
+#.This is a very important concept in computer programming. We need to deal with data all the time.
+#.A variable is a named place in the memory where a programmer can store data.
+#.This stored data can be later retrieved using the variable name. 
+#.Choosing a variable name is up to the programmer.
+#.You can even change the value of a variable at a later stage. 
+#.This is where a variable is different from a constant. Look at this example where you have the variables x and y and the values assigned are 25 and 50. 
 
