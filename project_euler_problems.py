61308e7945f236953121dacbb0892330a43d4b28
Virajbiz98 <Virajbiz98@gmail.com>
2024-10-10 03:16:52 +0530

    Create project_euler_problems.py

---
+++ project_euler_problems.py
@@ -0,0 +1,71 @@
+# ~ problem 01 
+#multiples of 3 or 5 
+#.if we list all the natural numbers below 10 that are multiples of 3 or 5 ,we get 3, 5,6 and 9 the sum of these multiples is 23.
+#. finde the sum of all the multiples of  or 5 below 1000.
+
+for i in range (10):
+  if(i % 3 == 0):
+    print(i)
+  elif(i % 5 == 0):
+    print(i)
+    sum = sum + i
+    print(i)
+    # .................
+for i in range (10):
+  if(i % 3 == 0):
+        print(i)
+  elif(i % 5 == 0):
+      print(i)
+  sum = sum + i:
+      print(i)
+#..........
+for i in range (10):
+    if(i % 3 == 0) and (i % == 5):
+      print(i)
+    sum = sum + i:
+      print(i)
+# ........................
+for i in range (10):
+  if(i % 3 == 0) or (i % 5 == 0):
+    print(i)
+  sum = sum + i:
+    print(i)
+
+# ..................
+for i in range (10):
+  if(i % 3 == 0) or (i % 5 == 0) = sum:
+    print(i)
+# ..........
+for i in range (10):
+  if(i % 3 == 0) sum (i % 5 == 0):
+    print(i)
+# ,,,,,,,,,,,,,,,,,
+for i in range (10):
+  if(i % 3 == 0) + (i % 5 == 0):
+sum =  sum + (i):
+     print(i)
+# ................
+for i in range (10):
+  if{[i % 3 == 0] + [i % 5 == 0]} = sum:
+    print(i)
+# ....................
+for i in range (10):
+  if((i % 3 == 0) or (i % 5 == 0)) and sum:
+     print(i)
+i = sum
+print(i)
+# ..............
+for i in range (10):
+  if((i % 3 == 0) or (i % 5 == 0)) and sum:
+    print(i)
+sum = sum + i:
+print(i)
+# .........
+for i in range (10):
+  if((i % 3 == 0) or (i % 5 == 0) and sum:
+    print(i)
+i = sum + i:
+print(i)
+# ..................
+    
+   
