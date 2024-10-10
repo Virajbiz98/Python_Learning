7a3f8934e9abbadd4909a5badd6eef004d8f26b2
Virajbiz98 <Virajbiz98@gmail.com>
2024-10-10 13:49:57 +0530

    Update project_euler_problems.py

--- project_euler_problems.py
+++ project_euler_problems.py
@@ -98,18 +98,16 @@ for i in range (10):
     if((i % 3 == 0 ) or (i % 5 == 0)):
         sum = sum + i
 
+print(sum)
 
 # ..finel 
-print(sum)
-        
 sum = 0
 for i in range (1000):
     if((i % 3 == 0 ) or (i % 5 == 0)):
         sum = sum + i
         
 print(sum)
-
-
+# ..>> 233168
 
 
 
