873467e3e50c24be9c7daf691859082a86afea62
Virajbiz98 <Virajbiz98@gmail.com>
2024-10-12 05:08:44 +0530

    Update 2nd_problem.py

--- project_euler_problems/2nd_problem.py
+++ project_euler_problems/2nd_problem.py
@@ -1,11 +1,15 @@
-a = 0
-b = 2
-c = a + b
+a = 0 + 1
+b = 1 + 1
+n = a + b
 sum = 0
-for i in range(10):
-    if(i >((a + b == 1 or b + c == 1) or (b + c == 2))):
-     
-     
-     
-     print(i)
-            
+def fibonacci(n):
+    if(n == a + b and b,a+b):
+        print(n)
+
+    
+    
+        
+ 
+sum = sum + n
+print(sum)
+
