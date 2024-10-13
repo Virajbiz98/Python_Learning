bac28404598c71ee0406d2bf4db541fa80588b4a
Virajbiz98 <Virajbiz98@gmail.com>
2024-10-13 14:41:21 +0530

    Update 2nd_problem.py

--- project_euler_problems/2nd_problem.py
+++ project_euler_problems/2nd_problem.py
@@ -1,8 +1,14 @@
-a, b, c, d, e, = 0, 1, 2, 3, 5, = n
+a = 0
+b = 1
+c = 1
+d = 2
+e = 3
+n = a + b , b + c, d + e, next 
 sum = 0
 for i in range(10):
     if(n % 2 == 0):
     elif(b + a == c , c + d == e):
+        print(n)
             sum = sum + n
             
 print(sum)            
