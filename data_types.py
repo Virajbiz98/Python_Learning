05dfe320f632ddc01bd7e13bdd3d990c3205c893
Virajbiz98 <Virajbiz98@gmail.com>
2024-10-15 19:18:30 +0530

    Create data_types.py

---
+++ data_types.py
@@ -0,0 +1,23 @@
+print(123) # numbers.- intijer(int)
+print(12.3) # floats-desimel numbers
+print("hello") # strings(str)
+print('good morning') # strings(str)
+print(True)
+print(False) #. Boolean values 
+print(2 + 3j) #.complex numbers
+print("hello \nworld") #. eacape charactors / new line
+>> hello 
+  world
+print("name\t: saman")
+print("cuntry\t: uganda") #. \t - print a tab 
+>> name  : saman
+cuntry   : uganda
+
+print("saman said \'good morning'") #.escape qouats
+>> sman saman said 'good morning'
+print("spacial character \n gives new line")
+>> special character
+   gives new line
+
+
+
