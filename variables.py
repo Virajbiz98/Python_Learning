604ed51581e069de3d3569e1985ecac15577505d
Virajbiz98 <Virajbiz98@gmail.com>
2024-10-16 11:24:56 +0530

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,61 @@
+name = 'saman' 
+print(name)
+>> saman
+a = 10
+b = 3
+print(a + b)
+>> 13
+print(a - b)
+>> 7 
+print(a * b)
+>> 30
+print(a / b)
+>> 3.333
+print(a // b) # floa division, purna sankyavta vatayai
+>> 3
+print(a  ** b) # a = 10 b = 3 , 10*10*10
+>>1000
+print(a % b) # module - It returns the remainder of dividing a by b
+>> 1
+
+a = "hello"
+b = "world"
+c = 5
+print(a + b)
+>> helloworld
+print(a * c)
+>>> hellohellohellohellohello
+
+a = "hello"
+b = "world"
+c = "5"
+d = a + " " + b + " " + c
+print(d)
+>> hello world 5
+
+
+a = "hello"
+b = "world"
+c = 5
+d = a + " " + b + " " + str(c)
+print(d)
+>> hello world 5
+
+a = True
+b = False
+
+age = 50
+
+is_yung = age <18
+
+print(is_yung)
+>> False
+
+
+
+
+
+
+
+
+
