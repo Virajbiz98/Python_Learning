f11ca2fc4b11f7ef177af85bfa8773a2a279f578
Virajbiz98 <Virajbiz98@gmail.com>
2024-10-25 12:04:58 +0530

    Create while loop.py

---
+++ Python-loops/while loop.py
@@ -0,0 +1,16 @@
+# ~whiile loop
+
+# A while loop repeats a block of code as long as a condition is True.
+# While loop kiyanne programming wala use karana repetitive statement ekak.specified condition ekak true wena thuru code block ekak execute karanna.
+
+# while condition:
+    # code to be executed
+# example for user input in while loop
+password = " "
+while password != "dev_testing":
+  password = input("enter the password: ")
+  if password != "dev_testing"
+  print("wrong password try again")
+
+print("correct password!")
+
