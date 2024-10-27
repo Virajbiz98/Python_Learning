4051a2419f8f23faa263712fce895ef3d9f89cac
Virajbiz98 <Virajbiz98@gmail.com>
2024-10-27 20:40:19 +0530

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,7 +1,7 @@
 name = input("Enter you name: ")
 
 def name_n():
-    if(name == "pattiya"):
+    if(name == "Jon"):
         print(f"hello {name}")
     else:
         print("You did not enter you name")
