6a6392102641b1a0d044e2dae63dff3d483b02b7
Virajbiz98 <Virajbiz98@gmail.com>
2024-10-27 19:35:27 +0530

    Update while loop.py

--- Python-loops/while loop.py
+++ Python-loops/while loop.py
@@ -1,16 +1,8 @@
-# ~whiile loop
+sum = 0
+i = 1;
+while(i<=5):
+    number =int(input("Enter Number : "))
+    sum = sum + number
+    i+=1
 
-# A while loop repeats a block of code as long as a condition is True.
-# While loop kiyanne programming wala use karana repetitive statement ekak.specified condition ekak true wena thuru code block ekak execute karanna.
-
-# while condition:
-    # code to be executed
-# example for user input in while loop
-password = " "
-while password != "dev_testing":
-  password = input("enter the password: ")
-  if password != "dev_testing"
-  print("wrong password try again")
-
-print("correct password!")
-
+print(f"Sum of numbers = {sum}")
