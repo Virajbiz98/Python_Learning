b0ed7bef69627ea8969f53685a0e84445035b209
Virajbiz98 <Virajbiz98@gmail.com>
2024-10-28 21:27:22 +0530

    Create Error_handling.py

---
+++ Error_handling.py
@@ -0,0 +1,12 @@
+# Syntax errors
+# Runtime errors
+# Logocal errors
+
+try:
+    a =  int(input("Enter first number: "))
+    b =  int(input("Enter second number: "))
+    print(a/b)
+
+except:
+    print("somthing went wrong")
+print("bye")
