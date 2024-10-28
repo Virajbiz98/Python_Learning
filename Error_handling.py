f72a79c42fcff419e5dd865c4947e04ffbbe7a2c
Virajbiz98 <Virajbiz98@gmail.com>
2024-10-28 21:51:26 +0530

    Update Error_handling.py

--- Error_handling.py
+++ Error_handling.py
@@ -1,12 +1,8 @@
-# Syntax errors
-# Runtime errors
-# Logocal errors
-
-try:
+try: 
     a =  int(input("Enter first number: "))
     b =  int(input("Enter second number: "))
     print(a/b)
 
-except:
-    print("somthing went wrong")
+except Exception as e: # e is chooseble and we can finde what is error using Exception 
+    print("somthing went wrong" ,e)
 print("bye")
