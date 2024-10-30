152f6ee2f5661b7b5ea6e0f6cdc157141bc9b382
Virajbiz98 <Virajbiz98@gmail.com>
2024-10-30 21:44:54 +0530

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,25 +1,4 @@
-# python calculator
+def say_hello(name):
+    print('Hey there', name)
 
-operator = input("Enter an operator (+ - * /): ")
-num1  = float(input("Enter the 1st number: "))
-num2  = float(input("Enter the 2st number: "))
-
-def cal_num():
-    if operator == "+":
-        result = num1 + num2
-        print(round(result, 3))
-    elif operator == "-":
-        result = num1 - num2
-        print(round(result, 3))
-    elif operator == "*":
-        result = num1 * num2
-        print(round(result, 3))
-    elif operator == "/":
-        result = num1 / num2
-        print(round(result, 3))
-    else:
-        print(f'{operator} is not valid')
-
-cal_num()
-
-
+say_hello('mark')
