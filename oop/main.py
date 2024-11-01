d50239d14efe2e2f469ebe25637082a8bea371af
Virajbiz98 <Virajbiz98@gmail.com>
2024-11-01 23:38:40 +0530

    Create main.py

---
+++ oop/main.py
@@ -0,0 +1,11 @@
+from Car import Car
+
+car_01 = Car("chevy","Corvette",2034,"blue")
+
+print(car_01.make)
+print(car_01.model)
+print(car_01.year)
+print(car_01.color)
+
+car_01.drive()
+car_01.stop()
