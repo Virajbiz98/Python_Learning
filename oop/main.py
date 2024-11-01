3b41ad9b739eb1f9e065cb89cbb2252a9245f528
Virajbiz98 <Virajbiz98@gmail.com>
2024-11-01 23:58:01 +0530

    Update main.py

--- oop/main.py
+++ oop/main.py
@@ -1,11 +1,7 @@
 from Car import Car
 
-car_01 = Car("chevy","Corvette",2034,"blue")
-
-print(car_01.make)
-print(car_01.model)
-print(car_01.year)
-print(car_01.color)
+car_01 = Car("chevy","range rover",2034,"blue")
+car_02 = Car("Ford","bmw",2025,"red")
 
 car_01.drive()
-car_01.stop()
+car_02.stop()
