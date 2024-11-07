34c23343af115f8eac57f20531ebf57ea051e3dd
Virajbiz98 <Virajbiz98@gmail.com>
2024-11-07 15:06:05 +0530

    Update while loop.py

--- Python-loops/while loop.py
+++ Python-loops/while loop.py
@@ -1,4 +1,9 @@
-i = 0
-while i < 5:
-    i = i + 1
-    print(i)
+import random
+
+h = random.randint(1,10)
+while True:
+    if h == (i:=int(input(":"))):
+        print("won")
+        break
+    print("Too high" if h > i else "Too low")
+    
