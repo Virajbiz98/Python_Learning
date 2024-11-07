fe4244279844220a23cdd9381d738165f6b3779d
Virajbiz98 <Virajbiz98@gmail.com>
2024-11-07 13:57:26 +0530

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,7 +1,10 @@
-def factorial(n):
-    if n == 0 or n == 1:
-        return 1
-    else:
-        return n * factorial(n - 1)
+# the code for short form  words
+def generator(sentence):
+    word_list = text.split()  # this for separate words
+    short_form = ""
+    for i in word_list:
+        short_form = short_form + i[0].upper() # .upper for capitalise text
+    print(short_form)
 
-print(factorial(5))
+text = input("Enter something: ")
+generator(text)
