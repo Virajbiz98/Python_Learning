68778d128dc9983a968efeca2f657db9bdd67cbe
Virajbiz98 <Virajbiz98@gmail.com>
2024-11-10 11:13:41 +0530

    Update exercise.py

--- exercise/exercise.py
+++ exercise/exercise.py
@@ -1,8 +1,3 @@
-# how to check if item is in list
-def is_group_number (group_data , n):
-    for value in group_data:
-        if n == value:
-            return True
-    return False
-
-print(is_group_number([1, 2, 3, 4, 5, 6, 6, 44] , 44))
+# How to reverse a number using slice operations in python
+num = input("Enter a number: ")
+print("Reverse of the number is " ,str(num) [::-1])
