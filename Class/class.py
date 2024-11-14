ec1fc0716ad33a864b7c121171c1c4c4aeefee45
Virajbiz98 <Virajbiz98@gmail.com>
2024-11-15 03:16:40 +0530

    init method

--- Class/class.py
+++ Class/class.py
@@ -1,13 +1,22 @@
-class Student:
-    def __init__(self,name,age):
-        self.name = name
-        self.age = age
+class Phone:
+  def __init__(self,make,model,year,color):
+    self.make = make
+    self.model = model
+    self.year = year
+    self.color = color
 
+  def cam(self) :
+      print("this " +self.model+ " phone have a good camara")
 
+  def temp(self):
+      print("this " +self.model+ " mobile is more expensive but is over heating sice using")
 
-st1 = Student("kamal",34)
-st2 = Student("Nimal",65)
-print(st1.name)
-print(st1.age)
-print(st2.name)
-print(st2.age)
+p1 = Phone("japan","sony",2023,"gray")
+p2 = Phone("amerika","apple",2023,"red")
+
+print(p1.make)
+print(p1.model)
+print(p1.year)
+print(p1.color)
+print(p1.cam())
+print(p2.temp())
