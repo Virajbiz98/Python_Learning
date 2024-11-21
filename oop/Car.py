bd835cc3bb4f253717f46277160889318b997abf
Virajbiz98 <Virajbiz98@gmail.com>
2024-11-21 19:16:05 +0530

    init method

--- oop/Car.py
+++ oop/Car.py
@@ -1,15 +1,19 @@
-class Car:
-
-
-    def __init__(self,make,model,year,color):
+class ac:
+    def __init__(self,make,model,year,color,):
         self.make = make
         self.model = model
         self.year = year
         self.color = color
 
-    def drive(self):
-        print("this  "+self.model+" is driving")
 
-    def stop(self):
-        print("this "+self.model+" is stopped")
+    def dura(self):
+        print(f"this " + self.model + " has good durability")
 
+
+    def maintain(self):
+        print(f"this " + self.model + "is easy maintain ")
+
+
+ac1 = ac("sri lanka","orange",2034,"white",)
+print(ac1.dura())
+print(ac1.make,ac1.model,ac1.year,ac1.color,ac1.maintain())
