3dcdbb0adc7c8daecc127652edccc6c8040ba5fc
Virajbiz98 <Virajbiz98@gmail.com>
2024-11-22 04:23:39 +0530

    Polymorphism

--- Class/class.py
+++ Class/class.py
@@ -1,12 +1,17 @@
-# Encapsulation (hiding data)
-class Fish:
-    def __init__(self,name,age,):
-        self.name = name
-        self.__age = age # privet attribute
-        
-    def get__age(self): # Public method to access the private attribute
-        return self.__age
-    
-fish = Fish("Dolphin", 16)
+# Polymorphism (Different Forms)
+class Animal:
+    def speak(self):
+        pass
 
-print(fish.get__age()) # accessing private attribute using a method
+class Dog(Animal):
+    def speak(self):
+        print("woof!")
+
+class Cat(Animal):
+    def speak(self):
+        print("meow!")
+
+# Creating a list of animals
+animals = [Dog(),Cat()]
+for animal in animals:
+    animal.speak() # # Each animal will speak its own way (Polymorphism)
