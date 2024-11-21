5863b1c5018e325d21d745b2118ce0a0df0709cc
Virajbiz98 <Virajbiz98@gmail.com>
2024-11-22 04:58:21 +0530

    str method

--- Class/class.py
+++ Class/class.py
@@ -1,11 +1,20 @@
-# magic method (special methods)
-class Dog:
-    def __init__(self,name,age):
-        self.name = name
-        self.age = age
+class Car:
+    def __init__(self,make,model,year):
+        self.make = make
+        self.model = model
+        self.year = year
+
+    def start_engine(self):
+        print(f"the engine of {self.year} {self.make} is now running.")
+
+    def stop(self):
+        print(f"The engine of {self.year} {self.make} is now off. ")
 
     def __str__(self):
-        return f"{self.name} is {self.age} years old."
+        return f"{self.year} {self.make} {self.model}"
 
-dog = Dog("Whaita" , 6)
-print(dog)
+#create Object 
+my_car = Car("Toyota", "corolla", 2026)
+print(my_car) # print the object using __str__ method
+my_car.start_engine()
+my_car.stop() #call the function
