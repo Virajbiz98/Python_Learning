e33a460869108a853caf587d68291f40f06c37b3
Virajbiz98 <Virajbiz98@gmail.com>
2024-11-23 07:27:17 +0530

    update inheritance

--- Class/class.py
+++ Class/class.py
@@ -1,13 +1,30 @@
-class Parent:
-    def func1(self):
-        print("hello")
+class Fruit:
+    number_of_items = None
+    unit_price = None
+    def set_value(self,x,y):
+        self.number_of_items = x
+        self.unit_price = y
 
-class Child(Parent):
-    def func2(self):
-        print("welcome")
+class Apple(Fruit):
+    def price(self):
+        print("for apple" ,self.number_of_items * self.unit_price)
 
-    def func1(self): #  overwriting method
-        print("hi")
+class Orange(Fruit):
+    def price(self):
+        print("for orange" ,self.number_of_items * self.unit_price)
 
-myobj1 = Child()
-myobj1.func1()
+class Grapes(Fruit):
+    def price(self):
+        print("for grapes" ,self.number_of_items * self.unit_price)
+
+myobj1 = Apple()
+myobj2 = Orange()
+myobj3 = Grapes()
+
+myobj1.set_value(12,34)
+myobj2.set_value(13,54)
+myobj3.set_value(15,4)
+
+myobj1.price()
+myobj2.price()
+myobj3.price()
