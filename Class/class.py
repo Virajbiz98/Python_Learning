5e859317a2f97735426228502defe5fa0bfe8aeb
Virajbiz98 <Virajbiz98@gmail.com>
2024-11-24 22:55:51 +0530

    instace method

--- Class/class.py
+++ Class/class.py
@@ -1,22 +1,11 @@
-# class variable 
-class Student:
-
-    class_year = 2024 
-    num_student = 0
-
-    def __init__(self,name,age):
+# instance method
+class Person:
+    def __init__(self,name):
+        print("Person is created.")
         self.name = name
-        self.age = age
-        Student.num_student += 1 # count objects 
 
-student1 = Student("mark",14)
-student2  = Student("patrick",16 )
-student3 = Student("saman", 17)
-student4 = Student("sandy", 14)
+    def print_name(self): 
+        print("Name is ", self.name)
 
-print(f"My graduating class of {Student.class_year} has {Student.num_student} students")
-print(student1.name)
-print(student2.name)
-print(student3.name)
-print(Student.class_year)
-
+praneeth = Person("praneeth")
+praneeth.print_name()
