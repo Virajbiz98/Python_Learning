10c56d6948f0bed8ff21d25eff2d13c0a7eb9361
Virajbiz98 <Virajbiz98@gmail.com>
2024-11-24 23:37:41 +0530

    class methods

--- Class/class.py
+++ Class/class.py
@@ -1,11 +1,21 @@
-# instance method
+
 class Person:
+
+    types = ["Student", "Teacher", "Librarian"] # class attributes. this  variables we can run without instance or with instance
+
     def __init__(self,name):
         print("Person is created.")
         self.name = name
 
     def print_name(self): 
+        print(self)
         print("Name is ", self.name)
+
+    @classmethod # decoraters.   
+    def get_types(cls):
+        return cls.types # can't access other functions only class variable
+
 
 praneeth = Person("praneeth")
 praneeth.print_name()
+print(Person.get_types()) 
