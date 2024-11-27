5041edca11456f7c71676e191a7e3cb7974b0631
Virajbiz98 <Virajbiz98@gmail.com>
2024-11-27 23:56:41 +0530

    streamlit

--- Python-loops/while loop.py
+++ Python-loops/while loop.py
@@ -1,9 +1,1 @@
-import random
 
-h = random.randint(1,10)
-while True:
-    if h == (i:=int(input(":"))):
-        print("won")
-        break
-    print("Too high" if h > i else "Too low")
-    
---
+++ streamlit_multipaje_app/views/about_me.py
@@ -0,0 +1,42 @@
+import streamlit as st 
+from forms.contact import contact_form
+
+@st.experimental_dialog("Contact Me")
+def show_contact_form():
+  contact_form()
+
+
+# --- ABOUT SECTION --- 
+col1,col2 = st.columns(2, gap="small",vertical_alignment="center")
+with col1:
+  st.image("assets/profile_image.png",width = 230)
+with col2:
+  st.title("Sven Bosau",anchor=False)
+  st.write(
+    "Seniior data Analyst, assisting enterprises by suporting data-driven decision-making." 
+  )
+  if st.button("✉ Contact Me"):
+    show_contact_form()
+
+# --- EXPEARIENCE AN QUALIFICATIONS ---
+st.write("\n")
+st.subheader("Expearence & Qualifications", anchor=False)
+st.write(
+  """
+  - 7 years expearience in python 
+  - Good understaning of statistical principles
+  - exelent team work
+  """
+)
+# --- SKILS ---
+st.write("\n")
+st.subheader("Hard skils", anchor=False)
+st.write(
+  """
+  -progamming python 
+  - data base : Mongodb
+  - content creating  
+  - data Visualization 
+  """
+)
+
