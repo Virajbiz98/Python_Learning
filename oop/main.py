503ef13bfb4c8fc437f2f68d6b4871e396561f1d
Virajbiz98 <Virajbiz98@gmail.com>
2024-11-27 07:54:42 +0530

    create a package

--- oop/main.py
+++
@@ -1,7 +0,0 @@
-from Car import Car
-
-car_01 = Car("chevy","range rover",2034,"blue")
-car_02 = Car("Ford","bmw",2025,"red")
-
-car_01.drive()
-car_02.stop()
---
+++ oop/package1/__init__.py
@@ -0,0 +1,6 @@
+print("hello")
+
+from .module1 import *
+from .module2 import *
+def func3():
+  print("this is from init file")
