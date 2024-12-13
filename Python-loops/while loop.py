dc34d08f16c5af93a09abcffd281ce0921fa2175
Virajbiz98 <Virajbiz98@gmail.com>
2024-12-13 23:55:06 +0530

    update while loop

--- Python-loops/while loop.py
+++ Python-loops/while loop.py
@@ -1,1 +1,11 @@
+dates = [1982, 1980, 1973, 2000]
 
+i = 0 
+year = 0
+
+while(year != 1973):
+    year = dates[i]
+    i = i + 1 
+    print(year)
+
+print("It took", i , "repetitions to out of loop.")
