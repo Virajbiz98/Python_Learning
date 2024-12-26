2381c4436ffd9c8b4188344e36b408c6bc6bb371
Virajbiz98 <Virajbiz98@gmail.com>
2024-12-26 21:15:35 +0530

    custom exception

--- Error_handling/Error_handling.py
+++ Error_handling/Error_handling.py
@@ -13,9 +13,9 @@
 #     print("bye")
 
 
-from os import path 
+# from os import path 
 
-file_name = 'Error_handling/data1.txt'
+# file_name = 'Error_handling/data1.txt'
 
 # if path.exists(file_name):
 
@@ -26,17 +26,17 @@ file_name = 'Error_handling/data1.txt'
 #     print("File doesn't exist")
 
 
-x = 10 
-y = 1 
+# x = 10 
+# y = 1 
 
 # z = x / y # it's geting error below this line how to run without crash
-try:
-    z = x / y 
-    print(z)
+# try:
+    # z = x / y 
+    # print(z)
 
-        # Can get FileNotFoundError
-    with open(file_name) as file:
-        print(file.readlines()) 
+    #     # Can get FileNotFoundError
+    # with open(file_name) as file:
+    #     print(file.readlines()) 
 
 # except (ZeroDivisionError, FileNotFoundError):
 #     print("input error")
@@ -44,16 +44,65 @@ try:
 # # except FileNotFoundError:
 # #     print("file not found")
 # except Exception:
-#     print("Something went wrong.")
+# #     print("Something went wrong.")
 
-except ZeroDivisionError as e: # we can asign a  variable
-    print("Input error", e)
+# except ZeroDivisionError as e: # we can asign a  variable
+#     print("Input error", e)
 
-except FileNotFoundError as e:
-    print("File not found",e)
+# except FileNotFoundError as e:
+#     print("File not found",e)
 
-except Exception as e:
-    print("Something wrong.", e)
+# except Exception as e:
+#     print("Something wrong.", e)
 
-finally:
-    print("Process competed.") # this impertent for cleanup task 
+# finally:
+#     print("Process competed.") # this impertent for cleanup task 
+
+# custom Exception 1
+class InvalidNameException(Exception):
+    def __init__(self, error):
+        super(InvalidNameException, self).__init__(error)
+        self.error_code = 10 
+
+# Custom Exception 2 
+class InvalidAgeException(Exception):
+    def __init__(self,error):
+        super(InvalidAgeException, self).__init__(error)
+        self.error_code =5
+
+class Person:
+    def __init__(self,name,age):
+        super().__init__()
+
+        self.name = name 
+        self.age = age 
+
+    @staticmethod
+    def get_person(name,age):
+        if not name:
+            # raise Exception("Invalid name")
+            raise InvalidNameException("Invalid Name") # this line for custom exception 1
+            print("Error")
+        if age < 0 or age >= 120:
+            # raise Exception("Invalid age")
+            raise InvalidAgeException("Invalid Age")
+            ruturn
+
+        return Person(name,age)
+try:
+    person = Person.get_person("Jayakodi", 30)
+    print(person)
+# except Exception as e:
+#     print("Error found",e)
+except(InvalidAgeException, InvalidNameException) as e:
+    if e.error_code == 10:
+        print("Invalid Name")
+    elif e.error == 5:
+        print("Invalid Age")
+    else:
+        print("somthing else went wrong.")
+    # print("Error found", e.error_code)
+
+print("hello world")
+
+
