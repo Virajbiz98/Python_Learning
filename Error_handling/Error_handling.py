6c04252ed9ce0b0ce8420b1599123304516d29e7
Virajbiz98 <Virajbiz98@gmail.com>
2024-12-26 21:03:43 +0530

    custom exception and raise keyword

--- Error_handling/Error_handling.py
+++ Error_handling/Error_handling.py
@@ -13,9 +13,9 @@
 #     print("bye")
 
 
-from os import path 
+# from os import path 
 
-file_name = 'Error_handling/data1.txt'
+# file_name = 'Error_handling/data1.txt'
 
 # if path.exists(file_name):
 
@@ -26,17 +26,17 @@ file_name = 'Error_handling/data1.txt'
 #     print("File doesn't exist")
 
 
-x = 10 
-y = 1 
+# x = 10 
+# y = 1 
 
 # z = x / y # it's geting error below this line how to run without crash
-try:
-    z = x / y 
-    print(z)
+# try:
+    # z = x / y 
+    # print(z)
 
-        # Can get FileNotFoundError
-    with open(file_name) as file:
-        print(file.readlines()) 
+    #     # Can get FileNotFoundError
+    # with open(file_name) as file:
+    #     print(file.readlines()) 
 
 # except (ZeroDivisionError, FileNotFoundError):
 #     print("input error")
@@ -44,16 +44,43 @@ try:
 # # except FileNotFoundError:
 # #     print("file not found")
 # except Exception:
-#     print("Something went wrong.")
+# #     print("Something went wrong.")
 
-except ZeroDivisionError as e: # we can asign a  variable
-    print("Input error", e)
+# except ZeroDivisionError as e: # we can asign a  variable
+#     print("Input error", e)
 
-except FileNotFoundError as e:
-    print("File not found",e)
+# except FileNotFoundError as e:
+#     print("File not found",e)
 
+# except Exception as e:
+#     print("Something wrong.", e)
+
+# finally:
+#     print("Process competed.") # this impertent for cleanup task 
+
+class Person:
+    def __init__(self,name,age):
+        super().__init__()
+
+        self.name = name 
+        self.age = age 
+
+    @staticmethod
+    def get_person(name,age):
+        if not name:
+            raise Exception("Invalid name")
+            print("Error")
+        if age < 0 or age >= 120:
+            raise Exception("Invalid age")
+            ruturn
+
+        return Person(name,age)
+try:
+    person = Person.get_person("Jayakodi", -30)
+    print(person)
 except Exception as e:
-    print("Something wrong.", e)
+    print("Error found",e)
 
-finally:
-    print("Process competed.") # this impertent for cleanup task 
+print("hello world")
+
+
