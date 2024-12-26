ba3bf3d9ebfd4ea1ea4477588fc2d374637fa5c5
Virajbiz98 <Virajbiz98@gmail.com>
2024-12-27 01:54:15 +0530

    ..

---
+++ Modules/main.py
@@ -0,0 +1,16 @@
+# import person as p
+import os.path as path
+import person 
+import sys 
+
+# print(sys.argv)
+# print(sys.path)
+print(sys.platform)
+
+# if path.exists('person.py'):
+    # print("Person is the")
+
+# print(__name__)
+# print(p.get_name())
+
+# print(dir(p))
---
+++ Modules/person.py
@@ -0,0 +1,11 @@
+name = "jayakodi"
+
+def get_name():
+    print(__name__)
+    return name 
+
+def get_age():
+    return 50 
+
+if __name__ == '__main__':
+    print("This is the entry point")
