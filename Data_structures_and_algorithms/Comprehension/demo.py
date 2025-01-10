fe479c2103ce5de0cfbb1eae843229bdb8a41f0a
Virajbiz98 <Virajbiz98@gmail.com>
2025-01-11 00:01:36 +0530

    Conditional Comprehension

--- Data_structures_and_algorithms/Comprehension/demo.py
+++ Data_structures_and_algorithms/Comprehension/demo.py
@@ -1,32 +1,23 @@
 def is_odd(number):
     return "odd" if number % 2 == 1 else "Even"
-    # return number % 2 == 1
-    # if number % 2 == 1:
-    #     return True
-    # return False
+    # return { number : "odd"} if number % 2 == 1 else { number : "Even"}
 
-
-
-a = [12, 45, 85, 33]
+a = [12, 45, 85, 87, 33, 100, 34, 11]
 b = []
-# b = a 
-# b = list(a)
 
-# is_odd(a)
-# b.append(100)
-for i in a:
-    b.append(i)
+# for i in a:
+#     b.append(i)
 
-b = [ is_odd(i) for i in a]
+# for i, value in enumerate(a):
+#     if i % 2 == 0:
+#         r = is_odd(value)
+#         b.append(r)
 
-# b = [i for i in a]
-# b = [i * 2 for i in a]
+# b = [is_odd(value) for i, value in enumerate(a) if i % 2 == 0 ]
+b = [ {value: is_odd(value)} for i, value in enumerate(a) if i % 2 == 0]
+
 
 print(a)
 print(b)
-# [12, 45, 85, 33]
-# [False, True, True, True]
 
-# [12, 45, 85, 33]
-# ['Even', 'odd', 'odd', 'odd']
 
