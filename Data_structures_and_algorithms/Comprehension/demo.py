5f768320b514315f8108a1ef8952062cbb9082e0
Virajbiz98 <Virajbiz98@gmail.com>
2025-01-11 11:16:47 +0530

    Other Comprehensions

--- Data_structures_and_algorithms/Comprehension/demo.py
+++ Data_structures_and_algorithms/Comprehension/demo.py
@@ -1,23 +1,15 @@
 def is_odd(number):
-    return "odd" if number % 2 == 1 else "Even"
-    # return { number : "odd"} if number % 2 == 1 else { number : "Even"}
+    return "Odd" if number % 2 == 1 else "Even"
 
-a = [12, 45, 85, 87, 33, 100, 34, 11]
+a = [12, 45, 87, 33, 100, 34, 11]
 b = []
 
-# for i in a:
-#     b.append(i)
+# b = { value for i, value in enumerate(a) if i % 2 == 0}
+# b = { value: is_odd(value) for i, value in enumerate(a) if i % 2 == 0}
 
-# for i, value in enumerate(a):
-#     if i % 2 == 0:
-#         r = is_odd(value)
-#         b.append(r)
-
-# b = [is_odd(value) for i, value in enumerate(a) if i % 2 == 0 ]
-b = [ {value: is_odd(value)} for i, value in enumerate(a) if i % 2 == 0]
-
+b = (value for i, value in enumerate(a) if i % 2 == 0)
 
 print(a)
-print(b)
 
-
+for i in b:
+    print(i)
