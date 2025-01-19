54bcd08cb6756c70ac6d540082911dbd68c007d4
Virajbiz98 <Virajbiz98@gmail.com>
2025-01-19 15:24:25 +0530

    updated my_thread.py

--- Multi_Processing/Sequencial_Downloader/main.py
+++ Multi_Processing/Sequencial_Downloader/main.py
@@ -1,5 +1,7 @@
 import requests 
 import time
+import my_thread 
+
 # http://picsum.photos/id/1/200/300
 
 def get_image_urls(count):
@@ -9,28 +11,39 @@ def get_image_urls(count):
         return 
 
     for i in range(0, count):
-        url = f'http://picsum.photos/id/{i}/200/300'
+        url = f'http://picsum.photos/id/{i}/1600/2400'
         yield url
+
 
 start = time.time_ns()
 
-urls = get_image_urls(100) 
-urls = get_image_urls(100) 
+urls = list(get_image_urls(100)) 
 
-for i, url in enumerate(urls):
-    r = requests.get(url, stream= True)
-    if r.status_code == 200:
-        r.raw.decode_content = True
-        file_name = f'images/{i}.jpg'
+urls_list = []
+num_threads = 10
+ 
+for i in range(0, len(urls),num_threads):
+    l = urls[i: i + num_threads]
+    urls_list.append(l)
 
-    with open(file_name,'wb') as f:
-        f.write(r.content)
+threads = []
 
-    print('Image sucessfully Downladed: ', file_name)
-else:
-    print('Image Couldn\'t beretreved')
+for i in range(0, num_threads):
+    thread = my_thread.ImageDownloader(i, f"Thread-{i}", urls_list[i])
+    thread.start() #run funtion
+    threads.append(thread)
+
+
+for thread in threads:
+    thread.join()
+
+
+
+# for i, url in enumerate(urls):
+#      download_image(url, i)
+
 
 diff = time.time_ns() - start
 
 print("Duration", diff / 1000000)
-# Duration 289994.102
+# # Duration 289994.102
