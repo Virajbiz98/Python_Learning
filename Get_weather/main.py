423297610490837f122bed5a3c69b36e64bdab85
Virajbiz98 <Virajbiz98@gmail.com>
2025-01-21 23:48:35 +0530

    Create main.py

---
+++ Get_weather/main.py
@@ -0,0 +1,9 @@
+city = 'colombo'
+api_key = 'a65b4ce65ab276307f712a666720c6'
+
+def get_weather_report(api_key, city):
+  url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
+
+if __name__ == '__main__':
+  print("Starting dynamic wallpaper")
+  
