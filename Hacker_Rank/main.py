ac6c7eb1156663a1a0e18ff24d525ffae3b3e92f
Virajbiz98 <Virajbiz98@gmail.com>
2025-01-25 14:32:38 +0530

    update main.py

--- Hacker_Rank/main.py
+++ Hacker_Rank/main.py
@@ -1,16 +1,13 @@
-# Task
+# The included code stub will read an integer, , from STDIN.
 
-# Given a year, determine whether it is a leap year. If it is a leap year, return the Boolean True, otherwise return False.
+# Without using any string methods, try to print the following:
+# 123...n
+# Note that "..." represents the consecutive values in between.
 
-# Note that the code stub provided reads from STDIN and passes arguments to the is_leap function. It is only necessary to complete the is_leap function.
-
-def is_leap(year):
-    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
-
-
-year = int(input())
-print(is_leap(year))
-
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(1, n+1):
+        print(i ,end="")
     
     
     
