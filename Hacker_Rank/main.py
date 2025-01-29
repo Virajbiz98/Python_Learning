15c35491055d5edf7672d5093f93e872dd7b4628
Virajbiz98 <Virajbiz98@gmail.com>
2025-01-29 20:27:14 +0530

    update main.py

--- Hacker_Rank/main.py
+++ Hacker_Rank/main.py
@@ -1,28 +1,10 @@
-# You are given three integers x, y, and z representing the dimensions of a cuboid along with an integer n. Print a list of all possible coordinates given by (i, j, k) on a 3D grid where the sum of i + j + k is not equal to n.
-# 0 ≤ i ≤ x
-# 0 ≤ j ≤ y
-# 0 ≤ k ≤ z
-# Please use list comprehensions rather than multiple loops, as a learning exercise.
+# Given the participants' score sheet for your University Sports Day, you are required to find the runner-up score. You are given n scores. Store them in a list and find the score of the runner-up.
+
 
 if __name__ == '__main__':
     n = int(input())
-    for i in range(1, n+1):
-        print(i ,end="")
+    scores =list(map(int, input().split()))
     
-    
-if __name__ == '__main__':
-    x = int(input())
-    y = int(input())
-    z = int(input())
-    n = int(input())
-    
-    result = [
-        [i, j, k]
-        for i in range (x + 1)
-        for j in range (y + 1)
-        for k in range (z + 1)
-        if i + j + k != n
-    ]
-    
-print(result)
-
+    max_score = max(scores)
+    runner_up = max([i for i in scores if i != max_score])
+    print(runner_up)
