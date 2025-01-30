00ed2f20ba7c39253d6ec1bd3d579dedf67f17cc
Virajbiz98 <Virajbiz98@gmail.com>
2025-01-30 22:15:28 +0530

    update while loop.py

--- Python-loops/while loop.py
+++ Python-loops/while loop.py
@@ -1,11 +1,7 @@
-dates = [1982, 1980, 1973, 2000]
+loop = True 
 
-i = 0 
-year = 0
-
-while(year != 1973):
-    year = dates[i]
-    i = i + 1 
-    print(year)
-
-print("It took", i , "repetitions to out of loop.")
+while loop:
+    password = input("Enter password: ")
+    if password == "stop":
+        break
+    
