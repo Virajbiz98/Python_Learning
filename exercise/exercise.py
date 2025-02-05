f2458e1c186753df4ca8c2e4c50079a02bf17afb
Virajbiz98 <Virajbiz98@gmail.com>
2025-02-05 23:18:11 +0530

    ..

--- exercise/exercise.py
+++ exercise/exercise.py
@@ -1,5 +1,18 @@
-# how to revers list
+# Exercise 13: Find the factorial of a given number
+# Write a Python program to use for loop to find the factorial of a given number.
 
-list = [ 100, 200, 300, 400, 500]
-list.reverse()
-print(list)
+# The factorial (symbol: !) means multiplying all numbers from the chosen number down to 1.
+
+# For example, a factorial of 5! is 5 × 4 × 3 × 2 × 1 = 120
+
+# Expected output:
+
+# 120
+
+n = 5
+factorial = 1 
+
+for i in range(1, n + 1):
+    factorial = factorial * i
+
+print("The factorial of", n , "is", factorial)
