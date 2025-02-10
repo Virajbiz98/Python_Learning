645d3de2e009f3524ea719e527b01914ed7dc63b
Virajbiz98 <Virajbiz98@gmail.com>
2025-02-11 01:52:52 +0530

    python graphics

--- exercise/exercise.py
+++ exercise/exercise.py
@@ -1,18 +1,18 @@
-# Exercise 13: Find the factorial of a given number
-# Write a Python program to use for loop to find the factorial of a given number.
+from turtle import *
+import colorsys
 
-# The factorial (symbol: !) means multiplying all numbers from the chosen number down to 1.
-
-# For example, a factorial of 5! is 5 × 4 × 3 × 2 × 1 = 120
-
-# Expected output:
-
-# 120
-
-n = 5
-factorial = 1 
-
-for i in range(1, n + 1):
-    factorial = factorial * i
-
-print("The factorial of", n , "is", factorial)
+speed(0)
+bgcolor("black")
+h = 0
+for i in range(16):
+    for j in range(18):
+        c = colorsys.hsv_to_rgb(h, 1, 1)
+        color(c)
+        h += 0.005
+        rt(90)
+        circle(150 - j * 6, 90)
+        lt(90)
+        circle(150 - j * 6, 90)
+        rt(180)
+    circle(40, 24)
+done()
