9b782984cedeec4b288a3ada7222b0e1eb2013c3
virajbiz98 <virajbiz98@gmail.com>
2025-03-26 20:00:28 +0500

    Remove unnecessary files and update api_key.py with new map URL and metric units; add requirements for flet and requests

--- Flet_App/api_key.py
+++ Flet_App/api_key.py
@@ -1,8 +1,12 @@
+
+
 API_KEY = "b03cd6345964bed883cd59e659a1d299"
+
 
 import requests 
 
 BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
+MAP_URL = "Https://www.openstreetmap.org/export/embed.html?bbox={lon}%2C{lat}%2C{lon}%2C{lat}&layer=mapnik"
 
 def get_weather(city):
     params = {"q": city, "appid": API_KEY, "units": "metric"}  # Changed "imperial" to "metric" for Celsius
