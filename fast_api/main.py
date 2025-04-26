ac3bcf0c9f6fbd0822705b01750a8242f3135eaa
virajbiz98 <virajbiz98@gmail.com>
2025-04-26 15:20:31 +0500

    Create simple FastAPI login with form handling

---
+++ fast_api/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import RedirectResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/")
+def home(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request})
+
+@app.post("/login")
+def login(request: Request, username: str = Form(...), password: str = Form(...)):
+    if username == "admin" and password == "1234":
+        return {"message": "Login Successful!"}
+    else:
+        return {"message": "Invalid Credentials!"}
